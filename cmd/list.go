b121b25db72c8e1380de13e1983d7fe492b95455
Junyu Wang <junyuw@berkeley.edu>
2018-06-02 16:27:25 -0700

    finished list command

--- cmd/list.go
+++ cmd/list.go
@@ -1,14 +1,33 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"corgi/config"
+	"corgi/snippet"
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
 
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "list all snippets",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		// Do stuff
-		return nil
-	},
+	RunE:  list,
+}
+
+func list(cmd *cobra.Command, args []string) error {
+	// load config
+	conf, err := config.Load()
+	if err != nil {
+		return err
+	}
+	// Load snippets
+	snippets, err := snippet.LoadSnippets(conf.SnippetsFile)
+	// display
+	fmt.Println("Here is the list of corgi snippets saved on your system:")
+	for _, s := range snippets.Snippets {
+		color.Yellow("[ %s ]", s.Title)
+	}
+	return nil
 }
 
 func init() {
