14a56d9113c76ccbeb55e1822872a0483a3433cc
Junyu Wang <junyuw@berkeley.edu>
2018-06-04 20:33:54 -0700

    added version

--- cmd/root.go
+++ cmd/root.go
@@ -5,12 +5,15 @@ import (
 	"os"
 )
 
+var appVersion = "v0.1.1-alpha"
+
 var rootCmd = &cobra.Command{
 	Use:   "corgi",
 	Short: "Corgi is a smart dog that helps you organize your command flow for future usage",
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
+	Version: appVersion,
 }
 
 func Execute() {
