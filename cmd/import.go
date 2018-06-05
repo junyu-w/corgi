64a0193f8e36e8f852c6e284d86898df1870c767
Junyu Wang <junyuw@berkeley.edu>
2018-06-04 21:26:35 -0700

    added import command

---
+++ cmd/import.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"corgi/snippet"
+	"github.com/spf13/cobra"
+)
+
+var importCmd = &cobra.Command{
+	Use:   "import",
+	Short: "Import a snippet from json file",
+	Args:  cobra.MinimumNArgs(1),
+	RunE:  importSnippet,
+}
+
+func importSnippet(cmd *cobra.Command, args []string) error {
+	// load config and snippets
+	conf, snippetsMeta, err := loadConfigAndSnippetsMeta()
+	if err != nil {
+		return err
+	}
+	// import snippet
+	snippetJsonFiles := args
+	for _, f := range snippetJsonFiles {
+		s, err := snippet.LoadSnippet(f)
+		if err != nil {
+			return err
+		}
+		if err = snippetsMeta.SaveNewSnippet(s, conf.SnippetsDir); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func init() {
+	rootCmd.AddCommand(importCmd)
+}
