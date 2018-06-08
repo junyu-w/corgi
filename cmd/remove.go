378e4065001f16d7d4e16b86797cba115010c54d
Junyu Wang <junyuw@berkeley.edu>
2018-06-07 22:14:15 -0700

    added remove command

---
+++ cmd/remove.go
@@ -0,0 +1,35 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var removeCmd = &cobra.Command{
+	Use:   "remove",
+	Short: "Remove a snippet",
+	Args:  cobra.MaximumNArgs(1),
+	RunE:  remove,
+}
+
+func remove(cmd *cobra.Command, args []string) error {
+	conf, snippetsMeta, err := loadConfigAndSnippetsMeta()
+	if err != nil {
+		return err
+	}
+	// find snippet title
+	var title string
+	if len(args) == 0 {
+		title, err = filterSnippetTitle(conf.FilterCmd, snippetsMeta.GetSnippetTitles())
+		if err != nil {
+			return err
+		}
+	} else {
+		title = args[0]
+	}
+	if err = snippetsMeta.DeleteSnippet(title); err != nil {
+		return err
+	}
+	return nil
+}
+
+func init() {
+	rootCmd.AddCommand(removeCmd)
+}
